c4ac1d94f8bc8c786f9e3c83990612b36adcd149
Nguyen Cao Ky <caoky2003xx@gmail.com>
2025-11-25 11:26:29 +0700

    feat(config): add jwt setttings

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,6 +4,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     uploads_folder: str
     sqlalchemy_database_url: str
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
+    refresh_token_expire_days: int
     model_config = SettingsConfigDict(env_file=".env")
 
 
