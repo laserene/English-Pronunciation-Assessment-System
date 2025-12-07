ff5034b7e4c35f6faf26a62c5e3e93bb1df44a27
Nguyen Cao Ky <caoky2003xx@gmail.com>
2025-12-07 14:31:17 +0700

    fix(invalid config): add missing groq_api_key attribute to Settings class to follow .env format

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     uploads_folder: str
     sqlalchemy_database_url: str
+    groq_api_key: str
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
