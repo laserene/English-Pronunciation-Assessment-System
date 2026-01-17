88a7abbd5b6d8820c697a2800ee58b10b0f36290
Nguyen Cao Ky <caoky2003xx@gmail.com>
2026-01-17 22:44:16 +0700

    feature(UI): add header UI

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -6,14 +6,6 @@ from app.auth.oauth2 import get_current_user
 router = APIRouter(prefix="/me", tags=["users"])
 
 
-@router.get("/protected", status_code=status.HTTP_200_OK)
-async def protected_route(current_user=Depends(get_current_user)):
-    """
-    A protected route to verify authentication.
-    """
-    return {"current_user": current_user}
-
-
 @router.get("/", status_code=status.HTTP_200_OK)
 async def get_current_user_info(current_user=Depends(get_current_user)):
     """
@@ -23,5 +15,4 @@ async def get_current_user_info(current_user=Depends(get_current_user)):
         "id": current_user.id,
         "username": current_user.username,
         "email": current_user.email,
-        # Add other fields you want to expose
     }
